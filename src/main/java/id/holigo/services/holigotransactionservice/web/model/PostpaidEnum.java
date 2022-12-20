fd6eb07640e950fff7734248c8d33d8fe7088511
ryo <nooryoku12331@gmail.com>
2022-12-20 22:51:52 +0700

    updating product enum

--- src/main/java/id/holigo/services/holigotransactionservice/web/model/PostpaidEnum.java
+++ src/main/java/id/holigo/services/holigotransactionservice/web/model/PostpaidEnum.java
@@ -2,6 +2,6 @@ package id.holigo.services.holigotransactionservice.web.model;
 
 public enum PostpaidEnum {
 
-    PAS, TLP, NETV, MFN, PAM, INS, CC, GAS, TF
+    PLNPOST, TLP, NETV, MFN, PAM, INS, CC, GAS, TF
 
 }
--- src/main/java/id/holigo/services/holigotransactionservice/web/model/PrepaidEnum.java
+++ src/main/java/id/holigo/services/holigotransactionservice/web/model/PrepaidEnum.java
@@ -2,6 +2,6 @@ package id.holigo.services.holigotransactionservice.web.model;
 
 public enum PrepaidEnum {
 
-    PUL, PD, PR, PRA, EWAL, GAME, DWAL, STREAM
+    PUL, PD, PR, PLNPRE, EWAL, GAME, DWAL, STREAM
 
 }
